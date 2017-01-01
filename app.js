4712b02e8fc7843ea8ce971c84772e030a8237dd
DanielReyns <Reyns.Daniel@gmail.com>
2017-01-01 18:59:27 +0100

    Try to add site to git

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var path = require('path');
+var connectSdk = require('connect-sdk-nodejs');
+
+var hostname = 'https://damp-brushlands-82940.herokuapp.com/'
+var portnumber = process.env.PORT || 8080;
+
+var app = express();
+app.set('view engine', 'ejs');
+
+var routes = require('./routes');
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+connectSdk.init({
+  host: hostname,
+  scheme: 'https',
+  port: portnumber,
+  enableLogging: true, // defaults to false
+  //logger: logger, // if undefined console.log will be used
+  apiKeyId: 'f76d4db20b15c030',
+  secretApiKey: 'B5rSIQn/rdpwYy8QmVTZ3d78rFq0Hecbnrc/DkwaUXE='
+});
+
+//Routes
+
+// home
+app.get('/', routes.home);
+
+// Card Single
+app.get('/card/:card_id?', routes.cardsingle);
+
+// notFound
+app.get('*', routes.notfound);
+
+app.listen(portnumber, function(){
+	console.log("The application is running on port " + portnumber)
+});
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var connectSdk = require('connect-sdk-nodejs');
+
+var hostname = 'https://damp-brushlands-82940.herokuapp.com/'
+var portnumber = process.env.PORT || 8080;
+
+connectSdk.init({
+  host: hostname,
+  scheme: 'https',
+  port: portnumber,
+  enableLogging: true, // defaults to false
+  //logger: logger, // if undefined console.log will be used
+  apiKeyId: 'f76d4db20b15c030',
+  secretApiKey: 'B5rSIQn/rdpwYy8QmVTZ3d78rFq0Hecbnrc/DkwaUXE='
+});
+
+
+var cardsJSON = require('../cards.json');
+
+// home
+exports.home = function(req, res) {
+
+	var cards = cardsJSON.cards;
+
+	res.render('home', {
+		title : "Magic Shop",
+		cards : cards
+	});
+};
+
+// Card Single
+exports.cardsingle = function(req, res) {
+	//var card_id = req.params.card_id;
+	//res.send("This is the page for card number " + card_id);
+	var body = {
+  			"hostedCheckoutSpecificInput": {"locale": "en_GB","variant": "101"}, 
+  			"order": {"amountOfMoney": {"currencyCode": "EUR", "amount": 1500}, 
+    		"customer": {"billingAddress": {"countryCode": "BE"}
+    		}
+  		}
+	};
+	connectSdk.hostedcheckouts.create("2508", body, null, function (error, sdkResponse) {});
+};
+
+// notFound
+exports.notfound = function(req, res) {
+	res.send("This is unknown magic");
+};
