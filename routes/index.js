9a1c32c5647f33734a7b58670967f0e2d33d3dac
DanielReyns <Reyns.Daniel@gmail.com>
2017-01-01 22:10:40 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,9 @@ exports.cardsingle = function(req, res) {
     		}
   		}
 	};
-	connectSdk.hostedcheckouts.create("2508", body, null, function (error, sdkResponse) {});
+	connectSdk.hostedcheckouts.create("2508", body, null, function (error, sdkResponse) {
+           render(res, error, sdkResponse);
+	});
 };
 
 // notFound
