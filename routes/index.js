092a3ff6d2d278d81fee4e75a66c00e1a2574228
DanielReyns <Reyns.Daniel@gmail.com>
2017-01-12 20:22:18 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -37,8 +37,9 @@ exports.cardsingle = function(req, res) {
   		}
 	};
 	connectSdk.hostedcheckouts.create("2508", body, null, function (error, sdkResponse) {
+		res.send("This is the page for card number " + card_id + " and the response was " + sdkResponse);
 	});
-	res.send("This is the page for card number " + card_id + " and the response was " + sdkResponse);
+	
 };
 
 // notFound
