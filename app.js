b1d24c634eb964151ba78e33b6bddfbc47cbca32
DanielReyns <Reyns.Daniel@gmail.com>
2017-01-13 00:28:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 var express = require('express');
 var path = require('path');
-var connectSdk = require('connect-sdk-nodejs');
-
-var hostname = 'api-sandbox.globalcollect.com'
 var portnumber = process.env.PORT || 8080;
-
-var app = express();
-app.set('view engine', 'ejs');
-
 var routes = require('./routes');
+var app = express();
 
-var path = require('path');
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-
-connectSdk.init({
-  host: hostname,
-  scheme: 'https',
-  //port: portnumber,
-  enableLogging: true, // defaults to false
-  //logger: logger, // if undefined console.log will be used
-  apiKeyId: 'f76d4db20b15c030',
-  secretApiKey: 'B5rSIQn/rdpwYy8QmVTZ3d78rFq0Hecbnrc/DkwaUXE=' 
-});
 
 //Routes
 
@@ -31,6 +15,7 @@ app.get('/', routes.home);
 // Card Single
 app.get('/card/:card_id?', routes.cardsingle);
 
+// Call Checkout with specific URL
 app.get('/finalize/:url?', routes.checkout);
 
 // notFound
